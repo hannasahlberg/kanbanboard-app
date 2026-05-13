4f8c765a8169654416c12e8d3b0afc9732558ec1
hannasahlberg <hannassahlberg@live.se>
2026-05-13 11:45:51 +0200

    gh-pages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BoardProvider } from "./context/BoardContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
 import ColumnPage from "./pages/ColumnPage";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200">
+    <BrowserRouter basename="/kanbanboard-app">
+      <div className="min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200">
         <Routes>
           <Route path="/" element={<BoardPage />} />
           <Route path="/column/:id" element={<ColumnPage />} />
         </Routes>
-        </div>
-
+      </div>
+    </BrowserRouter>
   );
 }
