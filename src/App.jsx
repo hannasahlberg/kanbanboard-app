bf0c7093ba7da770e9a1aca008349d8e831559b9
hannasahlberg <hannassahlberg@live.se>
2026-05-14 13:17:59 +0200

    browserrouter changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
 import ColumnPage from "./pages/ColumnPage";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/kanbanboard-app">
       <div className="min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200">
       <Routes>
         <Route path="/" element={<BoardPage />} />
         <Route path="/column/:id" element={<ColumnPage />} />
       </Routes>
       </div>
-      </BrowserRouter>
 
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BoardProvider } from './context/BoardContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BoardProvider>
+    <BrowserRouter basename="/kanbanboard-app">
     <App />
+    </BrowserRouter>
   </BoardProvider>
 )
